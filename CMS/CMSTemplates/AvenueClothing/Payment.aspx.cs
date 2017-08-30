5f6573b28c3d52f596601cc7835174f9ddf6f499
Leif Romme Thomsen <leif.romme.thomsen@ucommerce.net>
2017-08-30 13:26:46 +0200

    Simplified product page .js-file (star rating)

--- CMS/CMSTemplates/AvenueClothing/Payment.aspx.cs
+++ CMS/CMSTemplates/AvenueClothing/Payment.aspx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
-using CMS.Helpers;
 using CMS.UIControls;
 using UCommerce;
 using UCommerce.Api;
