4ce55c96dc1e35239d40c4f6dbc03f561a1d0442
Simon Broch Ulsted <devnull@localhost>
2017-09-01 11:21:33 +0200

    Added OrderlineDataSource files as well as modified UcommerceContext and UcommerceModule file to support Orderlines  - Actually added the files this time

---
+++ CMS/CMSWebParts/Ucommerce/UCommerceOrderlinesDataControl.ascx.cs
@@ -0,0 +1,51 @@
+﻿using CMS.DataEngine;
+using CMS.DocumentEngine.Web.UI;
+using CMS.Membership;
+using CMSApp.Old_App_Code.Custom;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CMSApp.CMSWebParts.Custom
+{
+    public partial class UCommerceOrderlinesDataControl : CMSBaseDataSource
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+        }
+
+        protected override object GetDataSource(int offset, int maxRecords)
+        {
+            var data = base.GetDataSource(offset, maxRecords) as List<UCommerceOrderline>;
+
+            UCommerceContext.SetOrderlines(data);
+
+            return data.ToDataSet();
+        }
+        protected override object GetDataSourceFromDB()
+        {
+            var ipad = new UCommerceOrderline
+            {
+                OrderlineId = "ipadpro97"
+            };
+            var iphone = new UCommerceOrderline
+            {
+                OrderlineId = "iphone7"
+            };
+            var macbook = new UCommerceOrderline
+            {
+                OrderlineId = "macbookpro",
+            };
+            var data = new List<UCommerceOrderline>
+            {
+                ipad,
+                iphone,
+                macbook
+            };
+
+            return data;
+
+        }
+    }
+}
