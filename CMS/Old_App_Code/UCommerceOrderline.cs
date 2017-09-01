39ac1356fe6a7f44157f40bbb318b85c43e5974b
Simon Broch Ulsted <devnull@localhost>
2017-09-01 09:24:10 +0200

    Orderline Data Source - Work in progress

---
+++ CMS/Old_App_Code/UCommerceOrderline.cs
@@ -0,0 +1,13 @@
+﻿using CMS.DataEngine;
+using System.Collections.Generic;
+using System;
+
+namespace CMSApp.Old_App_Code.Custom
+{
+    public class UCommerceProduct
+    {
+        public string ProductSKU { get; set; }
+        public string ProductName { get; set; }
+        public List<UCommerceProduct> RelatedProducts { get; set; }
+    }
+}
