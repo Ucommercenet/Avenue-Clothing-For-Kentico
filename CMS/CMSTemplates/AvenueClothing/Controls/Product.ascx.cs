1aa0c070ac3bcaf94bdd2481824aafc655dfa85c
davidKelemen <devnull@localhost>
2018-03-13 13:30:46 +0100

    Code formatting and re-added a control that was being used in search

---
+++ CMS/CMSTemplates/AvenueClothing/Controls/Product.ascx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UCommerce.Api;
+using UCommerce.Content;
+using UCommerce.Extensions;
+using UCommerce.Infrastructure;
+using UCommerce.Kentico.Content;
+using UCommerce.Runtime;
+
+namespace CMSApp.CMSTemplates.AvenueClothing.Controls
+{
+    public partial class Product : System.Web.UI.UserControl
+    {
+        public UCommerce.EntitiesV2.Product CurrentProduct { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var catalogContext = ObjectFactory.Instance.Resolve<ICatalogContext>();
+            var imageService = ObjectFactory.Instance.Resolve<IImageService>();
+
+            if (CurrentProduct == null)
+            {
+                Visible = false;
+                return;
+            }
+
+            string url = CatalogLibrary.GetNiceUrlForProduct(CurrentProduct, catalogContext.CurrentCategory);
+            PriceCalculation price = CatalogLibrary.CalculatePrice(CurrentProduct);
+
+            if (!string.IsNullOrWhiteSpace(CurrentProduct.ThumbnailImageMediaId))
+            {
+                productImage.ImageUrl = imageService.GetImage(CurrentProduct.ThumbnailImageMediaId).Url;
+                hlImage.Visible = true;
+            }
+
+            hlProduct.Text = CurrentProduct.DisplayName();
+            hlProduct.NavigateUrl = url;
+            hlViewMore.NavigateUrl = url;
+            hlImage.NavigateUrl = url;
+
+            litPrice.Text = price?.YourPrice?.Amount.ToString() ?? "-";
+
+            if (price?.YourTax != null)
+            {
+                litTax.Text = price.YourTax.ToString();
+            }
+        }
+    }
+}
--- CMS/CMSTemplates/AvenueClothing/Search.aspx
+++ CMS/CMSTemplates/AvenueClothing/Search.aspx
@@ -1,17 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CMSTemplates/AvenueClothing/Main.master" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="CMSApp.CMSTemplates.AvenueClothing.Search" %>
+
 <%@ Import Namespace="UCommerce.EntitiesV2" %>
 <%@ Register Src="~/CMSTemplates/AvenueClothing/Controls/Product.ascx" TagPrefix="uc1" TagName="Product" %>
-
 
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContentPlaceholder" runat="server">
     <div class="row-fluid">
         <div class="span12 product-list">
             <asp:ListView runat="server" ID="lvProducts">
                 <ItemTemplate>
-                    <uc1:Product runat="server" ID="Product" CurrentProduct="<%# (Product)Container.DataItem %>"/>
-    </ItemTemplate>
-    </asp:ListView>
+                    <uc1:Product runat="server" ID="Product" CurrentProduct="<%# (Product)Container.DataItem %>" />
+                </ItemTemplate>
+            </asp:ListView>
+        </div>
     </div>
-    </div>
-    </asp:Content>
-    
+</asp:Content>
